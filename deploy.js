b03dcae98469081d33cb40e56d09ee7cda488d9f
Subhransu Maharana <subhransumaharana@gmail.com>
2020-05-02 22:59:07 +0530

    update

--- deploy.js
+++
@@ -1,9 +0,0 @@
-var ghpages = require('gh-pages');
-
-// ghpages.publish('build', function (err) { });
-
-ghpages.publish('build', {
-    branch: 'master',
-    repo: 'https://github.com/subhset/shubharts.github.io.git'
-}, function (err) { console.log(err) });
-
