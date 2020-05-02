73a2358271fbc0d942ff741e57ce86511337475b
Subhransu Maharana <subhransumaharana@gmail.com>
2020-05-02 22:57:22 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import MyGallery from './Gallery/MyGallery';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <MyGallery />
+    </div>
+  );
+}
+
+export default App;
