bbb5fd541eec7a38f9cd81a99867a508ff52198e
smaharan <smaharan@adobe.com>
2020-06-14 00:51:15 +0530

    Update

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,13 +1,17 @@
-import irfan from '../images/full/irfan.jpg'
 import arya from '../images/full/arya.jpg'
-import joker from '../images/full/joker.jpg'
-import witcher from '../images/full/witcher.jpg'
-import baby from '../images/full/baby.jpg'
-import irfanTn from '../images/thumbnail/irfan_tn.jpg'
 import aryaTn from '../images/thumbnail/arya_tn.jpg'
+
+import joker from '../images/full/joker.jpg'
 import jokerTn from '../images/thumbnail/joker_tn.jpg'
+
+import witcher from '../images/full/witcher.jpg'
 import witcherTn from '../images/thumbnail/witcher_tn.jpg'
+
+import baby from '../images/full/baby.jpg'
 import babyTn from '../images/thumbnail/baby_tn.jpg'
+
+import irfan from '../images/full/irfan.jpg'
+import irfanTn from '../images/thumbnail/irfan_tn.jpg'
 
 import girl from '../images/full/girl.jpg'
 import girlTn from '../images/thumbnail/girl_tn.jpg'
@@ -20,10 +24,13 @@ import pantherTn from '../images/thumbnail/panther_tn.jpg'
 
 import faces from '../images/full/faces.jpg'
 import facesTn from '../images/thumbnail/faces_tn.jpg'
+
 import escape from '../images/full/escape.jpg'
 import escapeTn from '../images/thumbnail/escape_tn.jpg'
+
 import logan from '../images/full/logan.jpg'
 import loganTn from '../images/thumbnail/logan_tn.jpg'
+
 import lady from '../images/full/lady.JPG'
 import ladyTn from '../images/thumbnail/lady_tn.JPG'
 
@@ -36,106 +43,118 @@ import krishnaTn from '../images/thumbnail/krishna_tn.JPG'
 import scribbleMan from '../images/full/scribbleman.jpeg'
 import scribbleManTn from '../images/thumbnail/scribbleman_tn.jpeg'
 
+import elephant from '../images/full/elephant.jpg'
+import elephantTn from '../images/thumbnail/elephant.jpg'
+
 
 export const IMAGES =
-    [{
-        art_id: 0,
-        src: irfan,
-        thumbnail: irfanTn,
-        thumbnailWidth: 350,
-        thumbnailHeight: 400,
-        isSelected: true,
-        caption: "Irfan Khan"
-    },
-    {
-        art_id: 1,
-        src: arya,
-        thumbnail: aryaTn,
-        thumbnailWidth: 320,
-        thumbnailHeight: 400,
-        caption: "Not Today"
-    },
-
-    {
-        art_id: 2,
-        src: joker,
-        thumbnail: jokerTn,
-        thumbnailWidth: 320,
-        thumbnailHeight: 400,
-        caption: "Joker"
-    },
-    {
-        art_id: 14,
-        src: scribbleMan,
-        thumbnail: scribbleManTn,
-        caption: "Scribble Man"
-    },
-    {
-        art_id: 3,
-        src: witcher,
-        thumbnail: witcherTn,
-        thumbnailWidth: 500,
-        thumbnailHeight: 400,
-        caption: "The Witcher"
-    },
-    {
-        art_id: 4,
-        src: baby,
-        thumbnail: babyTn,
-        caption: "Quarantine Baby"
-    },
-    {
-        art_id: 5,
-        src: logan,
-        thumbnail: loganTn,
-        caption: "Logan"
-    },
-    {
-        art_id: 6,
-        src: faces,
-        thumbnail: facesTn,
-        caption: "Multi Faced"
-    },
-    {
-        art_id: 7,
-        src: girl,
-        thumbnail: girlTn,
-        caption: ""
-    },
-    {
-        art_id: 8,
-        src: khaleesi,
-        thumbnail: khaleesiTn,
-        caption: "Mother of Dragons"
-    },
-    {
-        art_id: 9,
-        src: escape,
-        thumbnail: escapeTn,
-        caption: "The Escape"
-    },
-    {
-        art_id: 10,
-        src: panther,
-        thumbnail: pantherTn,
-        caption: "Wakanda Forever"
-    },
-    {
-        art_id: 11,
-        src: lady,
-        thumbnail: ladyTn,
-        caption: ""
-    },
-    {
-        art_id: 12,
-        src: krishna,
-        thumbnail: krishnaTn,
-        caption: "Krishna"
-    },
-    {
-        art_id: 13,
-        src: jon,
-        thumbnail: jonTn,
-        caption: "White Walker"
-    }
+    [
+        {
+            art_id: 15,
+            src: elephant,
+            thumbnail: elephantTn,
+            thumbnailWidth: 350,
+            thumbnailHeight: 400,
+            isSelected: true,
+            caption: "RIP Elephant"
+        },
+        {
+            art_id: 0,
+            src: irfan,
+            thumbnail: irfanTn,
+            thumbnailWidth: 350,
+            thumbnailHeight: 400,
+            isSelected: true,
+            caption: "Irfan Khan"
+        },
+        {
+            art_id: 1,
+            src: arya,
+            thumbnail: aryaTn,
+            thumbnailWidth: 320,
+            thumbnailHeight: 400,
+            caption: "Not Today"
+        },
+        {
+            art_id: 2,
+            src: joker,
+            thumbnail: jokerTn,
+            thumbnailWidth: 320,
+            thumbnailHeight: 400,
+            caption: "Joker"
+        },
+        {
+            art_id: 14,
+            src: scribbleMan,
+            thumbnail: scribbleManTn,
+            caption: "Scribble Man"
+        },
+        {
+            art_id: 3,
+            src: witcher,
+            thumbnail: witcherTn,
+            thumbnailWidth: 500,
+            thumbnailHeight: 400,
+            caption: "The Witcher"
+        },
+        {
+            art_id: 4,
+            src: baby,
+            thumbnail: babyTn,
+            caption: "Quarantine Baby"
+        },
+        {
+            art_id: 5,
+            src: logan,
+            thumbnail: loganTn,
+            caption: "Logan"
+        },
+        {
+            art_id: 6,
+            src: faces,
+            thumbnail: facesTn,
+            caption: "Multi Faced"
+        },
+        {
+            art_id: 7,
+            src: girl,
+            thumbnail: girlTn,
+            caption: ""
+        },
+        {
+            art_id: 8,
+            src: khaleesi,
+            thumbnail: khaleesiTn,
+            caption: "Mother of Dragons"
+        },
+        {
+            art_id: 9,
+            src: escape,
+            thumbnail: escapeTn,
+            caption: "The Escape"
+        },
+        {
+            art_id: 10,
+            src: panther,
+            thumbnail: pantherTn,
+            caption: "Wakanda Forever"
+        },
+        {
+            art_id: 11,
+            src: lady,
+            thumbnail: ladyTn,
+            caption: ""
+        },
+        {
+            art_id: 12,
+            src: krishna,
+            thumbnail: krishnaTn,
+            caption: "Krishna"
+        },
+        {
+            art_id: 13,
+            src: jon,
+            thumbnail: jonTn,
+            caption: "White Walker"
+        }
     ]
