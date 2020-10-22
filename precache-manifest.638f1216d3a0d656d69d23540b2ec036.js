6aa9cbb886a479470d0276660e7fb92a953dd429
smaharan <smaharan@adobe.com>
2020-10-23 00:46:50 +0530

    Updates

---
+++ precache-manifest.638f1216d3a0d656d69d23540b2ec036.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0f8d9fd782c72d0eaf28158d79e131",
+    "url": "/shubharts/index.html"
+  },
+  {
+    "revision": "95f714060d38b06a09c6",
+    "url": "/shubharts/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "d201f2315446620e07a2",
+    "url": "/shubharts/static/js/2.48d9a509.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shubharts/static/js/2.48d9a509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f714060d38b06a09c6",
+    "url": "/shubharts/static/js/main.b3d8f74f.chunk.js"
+  },
+  {
+    "revision": "9b25bf51164cd1c75d04",
+    "url": "/shubharts/static/js/runtime-main.4acdb637.js"
+  },
+  {
+    "revision": "825fd16c50af8ed502bcfdbafd3097ba",
+    "url": "/shubharts/static/media/arya.825fd16c.jpg"
+  },
+  {
+    "revision": "66f063ac712ed113b212f96b56f0fd5e",
+    "url": "/shubharts/static/media/arya_tn.66f063ac.jpg"
+  },
+  {
+    "revision": "7d123feb8ae52a4ab585ecb382357dd4",
+    "url": "/shubharts/static/media/baby.7d123feb.jpg"
+  },
+  {
+    "revision": "e8ae3f2915a47c2319ec3b0b89db96c5",
+    "url": "/shubharts/static/media/baby_tn.e8ae3f29.jpg"
+  },
+  {
+    "revision": "7453984f982292dbdafe46724d752f88",
+    "url": "/shubharts/static/media/dark.7453984f.jpg"
+  },
+  {
+    "revision": "6a8dd3d2d4c1856394f62c0498ea75a8",
+    "url": "/shubharts/static/media/dark2_tn.6a8dd3d2.jpg"
+  },
+  {
+    "revision": "7482f6c33128238df49f52eef605cdc8",
+    "url": "/shubharts/static/media/elephant.7482f6c3.jpg"
+  },
+  {
+    "revision": "10195c016c1edc9c40bfd67db6506652",
+    "url": "/shubharts/static/media/elephant_tn.10195c01.jpg"
+  },
+  {
+    "revision": "6bc7be22dff512522ea45aa2ca68b132",
+    "url": "/shubharts/static/media/escape.6bc7be22.jpg"
+  },
+  {
+    "revision": "2b9e16eaa310a6315f143cdfa1de3857",
+    "url": "/shubharts/static/media/escape_tn.2b9e16ea.jpg"
+  },
+  {
+    "revision": "746b4ff86bd8f23dc4282ff6f5ad93df",
+    "url": "/shubharts/static/media/faces.746b4ff8.jpg"
+  },
+  {
+    "revision": "38c949bf42aa3c307b3c6b23da3a21ad",
+    "url": "/shubharts/static/media/faces_tn.38c949bf.jpg"
+  },
+  {
+    "revision": "e4dd22fee6dd59d9f67ac38382c95598",
+    "url": "/shubharts/static/media/girl.e4dd22fe.jpg"
+  },
+  {
+    "revision": "1b3c52bf719e266411242ecf7697f21b",
+    "url": "/shubharts/static/media/girl_tn.1b3c52bf.jpg"
+  },
+  {
+    "revision": "2935f156dec53490bb80f8abdf08945f",
+    "url": "/shubharts/static/media/irfan.2935f156.jpg"
+  },
+  {
+    "revision": "7cc508133c5bccf00c802affb490acb5",
+    "url": "/shubharts/static/media/irfan_tn.7cc50813.jpg"
+  },
+  {
+    "revision": "f4e24cb34fbbda564a676529f857c549",
+    "url": "/shubharts/static/media/joker.f4e24cb3.jpg"
+  },
+  {
+    "revision": "01b4103de107b353715060a02b33b4da",
+    "url": "/shubharts/static/media/joker_tn.01b4103d.jpg"
+  },
+  {
+    "revision": "932803ff8ea7f5544d8d7328f3e913ba",
+    "url": "/shubharts/static/media/jon.932803ff.jpg"
+  },
+  {
+    "revision": "932803ff8ea7f5544d8d7328f3e913ba",
+    "url": "/shubharts/static/media/jon_tn.932803ff.jpg"
+  },
+  {
+    "revision": "581f16dbfa14ede4483f8ffb541cbf66",
+    "url": "/shubharts/static/media/khaleesi.581f16db.jpg"
+  },
+  {
+    "revision": "259d4af627d93c826ab6d6fd5bf251db",
+    "url": "/shubharts/static/media/khaleesi_tn.259d4af6.jpg"
+  },
+  {
+    "revision": "5dd8338a3bcaa1432397127e3684a93c",
+    "url": "/shubharts/static/media/krishna-patachitra.5dd8338a.jpg"
+  },
+  {
+    "revision": "0f293382efa3128a752ba0f0c8ce8fca",
+    "url": "/shubharts/static/media/krishna-patachitra_tn.0f293382.jpg"
+  },
+  {
+    "revision": "09b78f3d36301ed15cdd7494b8a58b4e",
+    "url": "/shubharts/static/media/krishna.09b78f3d.JPG"
+  },
+  {
+    "revision": "facc04a388a94057e5331c84941e21d8",
+    "url": "/shubharts/static/media/krishna_tn.facc04a3.JPG"
+  },
+  {
+    "revision": "e52af30be339156591b979ad98e7f60d",
+    "url": "/shubharts/static/media/lady.e52af30b.JPG"
+  },
+  {
+    "revision": "8756fddc2a70d274985d513dc3e77d1d",
+    "url": "/shubharts/static/media/lady_tn.8756fddc.JPG"
+  },
+  {
+    "revision": "5fcbc7d7cdd47d7c6c5463e2b82e40e2",
+    "url": "/shubharts/static/media/logan.5fcbc7d7.jpg"
+  },
+  {
+    "revision": "5fcbc7d7cdd47d7c6c5463e2b82e40e2",
+    "url": "/shubharts/static/media/logan_tn.5fcbc7d7.jpg"
+  },
+  {
+    "revision": "22f974897be235cfdb024a863fbf6a44",
+    "url": "/shubharts/static/media/panther.22f97489.jpg"
+  },
+  {
+    "revision": "95df72ccb7ddaa06411219cf0647f9e3",
+    "url": "/shubharts/static/media/panther_tn.95df72cc.jpg"
+  },
+  {
+    "revision": "09c68d79514956999b68ba42b0098292",
+    "url": "/shubharts/static/media/scribbleman.09c68d79.jpeg"
+  },
+  {
+    "revision": "4f86c2cbed65d386d17728059e0c0b58",
+    "url": "/shubharts/static/media/scribbleman_tn.4f86c2cb.jpeg"
+  },
+  {
+    "revision": "ef0815e06ac2e5b9ce53b02f0c4f781f",
+    "url": "/shubharts/static/media/witcher.ef0815e0.jpg"
+  },
+  {
+    "revision": "91bbde3041c2a6dc363c6e2238eade17",
+    "url": "/shubharts/static/media/witcher_tn.91bbde30.jpg"
+  }
+]);
